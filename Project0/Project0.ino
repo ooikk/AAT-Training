cbfb4b70a23f98fd3e9933c49d3871855925c788
ooikk <105415945+ooikk@users.noreply.github.com>
2026-06-25 12:13:41 +0800

    Update Project0.ino

--- Project0/Project0.ino
+++ Project0/Project0.ino
@@ -8,7 +8,7 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   delay(1000);
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
